eaf763dec697b80e61d890a0eb8dac017e7b4c61
bitr3 <bitr3@DESKTOP-IMERMO8>
2020-12-11 19:47:39 +0900

    java practice04 과제

--- Practice04/src/main/java/prob1/Prob1.java
+++ Practice04/src/main/java/prob1/Prob1.java
@@ -15,7 +15,7 @@ public class Prob1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
 		while(true) {
-			int sum=0;
+			int sum=0; 
 			System.out.print( "숫자를 입력하세요. : " );
 			int inputNumber = scanner.nextInt();
 				
